03a2c5a1bf8da15e5e488e0d3c0e64387d055327
Sanooda Abeysinghe <168185705+sanuwa13@users.noreply.github.com>
2026-07-22 15:48:50 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteListing } from '../controllers/listing.controller.js';
+import { deleteListing, updateListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
