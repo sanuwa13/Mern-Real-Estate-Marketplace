7bc707086974ffbee7f4e71fe8b356f83fdd8cd2
Sanooda Abeysinghe <168185705+sanuwa13@users.noreply.github.com>
2026-07-22 14:55:02 +0530

    complete user delete functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,12 +1,33 @@
-import Listing from '../models/listing.model.js';
+import Listing from "../models/listing.model.js";
 
-export const createListing = async(req, res, next) => {
-    try {
-        const listing = await Listing.create(req.body);
-        res.status(201).json(listing);
-    } catch (error) {
-        next(error);
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      const err = new Error("Listing not found!");
+      err.statusCode = 404;
+      return next(err);
     }
 
+    if (req.user.id !== listing.userRef) {
+      const err = new Error("You can only delete your own listings!");
+      err.statusCode = 401;
+      return next(err);
+    }
 
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
